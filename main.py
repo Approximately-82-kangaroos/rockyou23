211bf6049dd84d2e07b57b17b0f188baab6ee488
Evan Skyberg <evnskg@proton.me>
2023-11-08 10:36:11 -0700

    Updated the concatenation script

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import math
 
 if (__name__ == "__main__"):
     urlList = []
@@ -8,23 +9,27 @@ if (__name__ == "__main__"):
     for line in filefile:
         urlList.append(line.strip())
     
-    passwordList = ["password"]
+    passwordList = []
 
+    time2 = time.time()
     for url in urlList:
-        r = requests.get(url, allow_redirects = True)
+        r = requests.get(url)
         open("temp.txt", "w").write(r.content.decode("latin-1"))
         tempFile = open("temp.txt", "r")
         time1 = time.time()
-        time2 = time.time()
         for line in tempFile:
             line = line.strip()
             if (line not in passwordList):
                 passwordList.append(line)
                 if (len(passwordList) % 10000 == 0):
-                    print(f"{len(passwordList)}\t{10000/(time.time() - time1)}\t{len(passwordList)/(time.time() - time2)}")
+                    time3 = time.time() - time2
+                    if (time3 >= 60):
+                        print(f"{len(passwordList)}\t{10000/(time.time() - time1)}\t{math.floor(time3/60)} minutes {time3 % 60} seconds")
+                    elif (time3 > 3600):
+                        print(f"{len(passwordList)}\t{10000/(time.time() - time1)}\t{math.floor(time3/3600)} hours {math.floor((time3 % 3600) / 60)} minutes {time3 % 60} seconds")
+                    else:
+                        print(f"{len(passwordList)}\t{10000/(time.time() - time1)}\t{time3} seconds")
                     time1 = time.time()
-            else:
-                print(f"Skipped {line}")
         print(len(passwordList))
     
     passwordFile = open("rockyou23.txt", "a")
