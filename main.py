337da0eefde9c3729f409cb6a0492ce0d44443df
Evan Skyberg <evnskg@proton.me>
2023-11-08 22:09:19 -0700

    Updated the code

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if (__name__ == "__main__"):
     for line in filefile:
         urlList.append(line.strip())
     
-    passwordList = []
+    passwordList = set()
 
     time2 = time.time()
     for url in urlList:
@@ -19,17 +19,16 @@ if (__name__ == "__main__"):
         time1 = time.time()
         for line in tempFile:
             line = line.strip()
-            if (line not in passwordList):
-                passwordList.append(line)
-                if (len(passwordList) % 10000 == 0):
-                    time3 = time.time() - time2
-                    if (time3 >= 3600):
-                        print(f"{len(passwordList)}\t{10000/(time.time() - time1)}\t{math.floor(time3/3600)} hours {math.floor((time3 % 3600) / 60)} minutes {time3 % 60} seconds")
-                    elif (time3 >= 60):
-                        print(f"{len(passwordList)}\t{10000/(time.time() - time1)}\t{math.floor(time3/60)} minutes {time3 % 60} seconds")
-                    else:
-                        print(f"{len(passwordList)}\t{10000/(time.time() - time1)}\t{time3} seconds")
-                    time1 = time.time()
+            passwordList.add(line)
+            if (len(passwordList) % 10000 == 0):
+                time3 = time.time() - time2
+                if (time3 >= 3600):
+                    print(f"{len(passwordList)}\t{10000/(time.time() - time1)}\t{math.floor(time3/3600)} hours {math.floor((time3 % 3600) / 60)} minutes {time3 % 60} seconds")
+                elif (time3 >= 60):
+                    print(f"{len(passwordList)}\t{10000/(time.time() - time1)}\t{math.floor(time3/60)} minutes {time3 % 60} seconds")
+                else:
+                    print(f"{len(passwordList)}\t{10000/(time.time() - time1)}\t{time3} seconds")
+                time1 = time.time()
         print(len(passwordList))
     
     passwordFile = open("rockyou23.txt", "a")
