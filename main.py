b1dc3f10e84dffe54df902ab2e8764b6ecb00768
Evan Skyberg <evnskg@proton.me>
2023-11-08 20:07:36 -0700

    Edited concatenation script

--- main.py
+++ main.py
@@ -23,10 +23,10 @@ if (__name__ == "__main__"):
                 passwordList.append(line)
                 if (len(passwordList) % 10000 == 0):
                     time3 = time.time() - time2
-                    if (time3 >= 60):
-                        print(f"{len(passwordList)}\t{10000/(time.time() - time1)}\t{math.floor(time3/60)} minutes {time3 % 60} seconds")
-                    elif (time3 > 3600):
+                    if (time3 >= 3600):
                         print(f"{len(passwordList)}\t{10000/(time.time() - time1)}\t{math.floor(time3/3600)} hours {math.floor((time3 % 3600) / 60)} minutes {time3 % 60} seconds")
+                    elif (time3 >= 60):
+                        print(f"{len(passwordList)}\t{10000/(time.time() - time1)}\t{math.floor(time3/60)} minutes {time3 % 60} seconds")
                     else:
                         print(f"{len(passwordList)}\t{10000/(time.time() - time1)}\t{time3} seconds")
                     time1 = time.time()
