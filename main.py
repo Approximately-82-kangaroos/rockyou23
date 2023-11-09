8547214215367aba363daf250c26c3a66a1e1acc
Evan Skyberg <evnskg@proton.me>
2023-11-09 13:14:30 -0700

    Updated cat script & file list

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-import time
 import math
 
 if (__name__ == "__main__"):
@@ -11,17 +10,25 @@ if (__name__ == "__main__"):
     
     passwordList = set()
 
-    time2 = time.time()
     for url in urlList:
-        r = requests.get(url)
-        open("temp.txt", "w").write(r.content.decode("latin-1"))
-        tempFile = open("temp.txt", "r")
-        time1 = time.time()
+        if (url[:1] == "#"):
+            continue
+        print(url)
+        if (url[:4] == "http"):
+            r = requests.get(url)
+            open("temp.txt", "w").write(r.content.decode("latin-1"))
+            tempFile = open("temp.txt", "r")
+        else:
+            tempFile = open(url, "r")
         for line in tempFile:
             line = line.strip()
             passwordList.add(line)
         print(len(passwordList))
+        tempFile.close()
     
+    passwordFile = open("hurricane.txt", "w")
+    passwordFile.write("")
+    passwordFile.close()
     passwordFile = open("hurricane.txt", "a")
     for i in passwordList:
         passwordFile.write(f"{i}\n")
